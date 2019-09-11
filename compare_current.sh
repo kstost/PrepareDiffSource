cae05dd88a43cd82ea204a3654c7c6dda39edfbd
kst <kstost@gmail.com>
2019-09-11 10:31:46 +0900

    Update compare_current.sh

--- compare_current.sh
+++ compare_current.sh
@@ -1,1 +1,5 @@
-python prepare_diff_source.py kstost $(basename `pwd`) `pwd` 0 
+#!/bin/bash
+# https://www.lesstif.com/pages/viewpage.action?pageId=26083916
+if [ "$#" -eq 1 ]; then
+    python prepare_diff_source.py kstost $(basename `pwd`) `pwd` $1
+fi
